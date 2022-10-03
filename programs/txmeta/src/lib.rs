1e277f68ac6ef92f3bc671baaa21a57d4a239bac
Sean <sean@organgrindermedia.com>
2022-10-03 11:16:39 -0400

    fix(txmeta): output accounts

--- programs/txmeta/src/lib.rs
+++ programs/txmeta/src/lib.rs
@@ -5,13 +5,18 @@ declare_id!("SMPL5bz5ERMdweouWrXtk3jmb6FnjZkWf7pHDsE6Zwz");
 pub mod txmeta {
     use super::*;
 
-    pub fn track_meta(_ctx: Context<Track>) -> Result<()> {
+    pub fn track_meta(ctx: Context<Track>, meta: String) -> Result<()> {
+        let remaining = ctx.remaining_accounts;
+        remaining.iter().for_each(|account| {
+            msg!("Account: {:?}", account.key());
+        });
+        msg!("Track Meta: {:?}", meta);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(meta: String)]
 pub struct Track<'info>{
+    #[account(mut)]
     pub member: Signer<'info>,
 }
