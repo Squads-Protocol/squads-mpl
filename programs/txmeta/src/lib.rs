3f8480ba2e44b9dbaccaae4efa1975305eb820c6
RohanXKapur <rohankapurdev@outlook.com>
2022-10-27 01:17:22 +0800

    add MsTransaction validation to track_meta

--- programs/txmeta/src/lib.rs
+++ programs/txmeta/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use squads_mpl::state::Ms;
+use squads_mpl::state::{Ms, MsTransaction};
 declare_id!("SMPL5bz5ERMdweouWrXtk3jmb6FnjZkWf7pHDsE6Zwz");
 
 #[program]
@@ -24,17 +24,28 @@ pub struct Track<'info> {
 
     #[account(constraint = multisig.to_account_info().owner == &squads_mpl::ID @ ErrorCode::InvalidOwner)]
     pub multisig: Account<'info, Ms>,
+
+    #[account(constraint = transaction.to_account_info().owner == &squads_mpl::ID @ ErrorCode::InvalidOwner)]
+    pub transaction: Account<'info, MsTransaction>,
 }
 
 impl<'info> Track<'info> {
-    /// Check to make sure that the signer is a member of the supplied [squads_mpl::state::Ms] account
     fn validate(ctx: &Context<Track>) -> Result<()> {
         let multisig = &ctx.accounts.multisig;
         let signer = &ctx.accounts.member;
+        let transaction = &ctx.accounts.transaction;
 
+        // Check to make sure that the signer is a member of the supplied [squads_mpl::state::Ms] account
         match multisig.is_member(signer.key()) {
-            Some(_) => Ok(()),
-            None => Err(ErrorCode::Unauthorized.into()),
+            Some(_) => {
+                // Check to make sure the signer is the creator of the supplied [squads_mpl::state::MsTransaction] account
+                // and that the transaction comes from the supplied [squads_mpl::state::Ms] account
+                if transaction.creator == signer.key() && transaction.ms == multisig.key() {
+                    return Ok(());
+                };
+                return err!(ErrorCode::InvalidTransaction);
+            }
+            None => err!(ErrorCode::Unauthorized),
         }
     }
 }
@@ -43,6 +54,8 @@ impl<'info> Track<'info> {
 pub enum ErrorCode {
     #[msg("Signer is not a member of the specified multisig.")]
     Unauthorized,
-    #[msg("The owner of the multisig account is not the expected program.")]
+    #[msg("The owner of the account is not the expected program.")]
     InvalidOwner,
+    #[msg("The transaction is either not associated with the supplied multisig or it's creator is not the supplied signer")]
+    InvalidTransaction,
 }
